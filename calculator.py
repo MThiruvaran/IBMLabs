2a9cfad5358a985386d843f8ac846a240890a0bf
MThiruvaran <41546415+MThiruvaran@users.noreply.github.com>
2020-02-10 22:23:40 -0800

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,8 @@
+num1 = int(input("enter the number:"))
+num2 = int(input("enter another number:"))
+
+print('the sum of {0} and {1} is {2}'.format(num1, num2, num1 + num2))
+print('the difference between {0} and {1} is {2}'.format(num1, num2, num1 - num2))
+print('the product of {0} and {1} is {2}'.format(num1, num2, num1 * num2))
+print('the quotient of {0} and {1} is {2}'.format(num1, num2, float(num1 / num2)))
+print('the remainder of {0} and {1} is {2}'.format(num1, num2, float(num1 % num2)))
